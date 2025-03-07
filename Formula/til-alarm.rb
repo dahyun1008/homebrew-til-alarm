ed81437ba9b2c3d251aa94e3fa4682dd280c22e6
dahyun <gimd15629@gmail.com>
2025-03-07 17:31:09 +0900

    패키지명 수정

--- Formula/til-alarm.rb
+++ Formula/til-alarm.rb
@@ -1,4 +1,4 @@
-class TIL-Alarm < Formula
+class til-alarm < Formula
     desc "오늘 TIL을 작성하지 않았다면 팝업 알람을 보내드립니다."
     homepage "https://github.com/dahyun1008/homebrew-til-alarm"
     url "https://github.com/dahyun1008/homebrew-til-alarm/releases/tag/0.1.0"
