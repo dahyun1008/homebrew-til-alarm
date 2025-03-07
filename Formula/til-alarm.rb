a5a773fa7722de7066f4b99e2e076b6b028d13dc
dahyun <gimd15629@gmail.com>
2025-03-07 17:27:54 +0900

    루비 파일 추가

---
+++ Formula/til-alarm.rb
@@ -0,0 +1,10 @@
+class TIL-Alarm < Formula
+    desc "오늘 TIL을 작성하지 않았다면 팝업 알람을 보내드립니다."
+    homepage "https://github.com/dahyun1008/homebrew-til-alarm"
+    url "https://github.com/dahyun1008/homebrew-til-alarm/releases/tag/0.1.0"
+    sha256 "파일의 SHA256 값"
+
+    def install
+        bin.install "til-alarm"
+    end
+end
