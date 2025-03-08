0199bc0e285fb69d7fd83b2d0511979466ab6aca
dahyun <gimd15629@gmail.com>
2025-03-08 09:27:01 +0900

    실행파일 경로 url 설정

--- Formula/til-alarm.rb
+++ Formula/til-alarm.rb
@@ -1,7 +1,7 @@
 class TilAlarm < Formula
     desc "오늘 TIL을 작성하지 않았다면 팝업 알람을 보내드립니다."
     homepage "https://github.com/dahyun1008/homebrew-til-alarm"
-    url "https://github.com/dahyun1008/homebrew-til-alarm/releases/tag/0.1.0"
+    url "https://github.com/dahyun1008/homebrew-til-alarm/releases/download/0.1.0/til-alarm"
     def install
         bin.install "til-alarm"
     end
