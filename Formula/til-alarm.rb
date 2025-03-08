1fbcbf05b3e0a24ed302283ab7c4b7e037d4266b
dahyun <gimd15629@gmail.com>
2025-03-08 14:13:58 +0900

    실행파일 추가하기

--- Formula/til-alarm.rb
+++ Formula/til-alarm.rb
@@ -1,9 +1,12 @@
 class TilAlarm < Formula
     desc "오늘 TIL을 작성하지 않았다면 팝업 알람을 보내드립니다."
     homepage "https://github.com/dahyun1008/homebrew-til-alarm"
-    url "https://github.com/dahyun1008/homebrew-til-alarm/releases/download/0.1.0/til-alarm"
+    url "https://github.com/dahyun1008/homebrew-til-alarm/releases/download/0.1.1/til-alarm"
     def install
         bin.install "til-alarm"
         bin.install "til-alarm-crontab"
+
+        chmod "+x", bin/"til-alarm"
+        chmod "+x", bin/"til-alarm-crontab"
     end
 end
