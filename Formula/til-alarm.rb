3a1709e511880f195d40a644b76419ee15a021af
dahyun <gimd15629@gmail.com>
2025-03-08 14:45:29 +0900

    압축 파일 관련 경로 수정

--- Formula/til-alarm.rb
+++ Formula/til-alarm.rb
@@ -1,10 +1,10 @@
 class TilAlarm < Formula
     desc "오늘 TIL을 작성하지 않았다면 팝업 알람을 보내드립니다."
     homepage "https://github.com/dahyun1008/homebrew-til-alarm"
-    url "https://github.com/dahyun1008/homebrew-til-alarm/releases/download/0.1.1/til-alarm"
+    url "https://github.com/dahyun1008/homebrew-til-alarm/releases/download/0.1.1/til-alarm-0.1.1.tar.gz"
     def install
-        bin.install "til-alarm"
-        bin.install "til-alarm-crontab"
+        bin.install "bin/til-alarm"
+        bin.install "bin/til-alarm-crontab"
 
         chmod "+x", bin/"til-alarm"
         chmod "+x", bin/"til-alarm-crontab"
